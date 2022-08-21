4b4ff7e906460363faed2782dc4ef498a88ee8ec
Anastasiyu <asy.safiulina@mail.ru>
2022-08-21 21:26:56 +0300

    ДЗ Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        byte box = 2;
+        short apple = 230;
+        int home = 35000;
+        long big =1235649853;
+        float jou = 2.5F;
+        double x = 5.5;
+        char y = 0;
+        boolean z = true;
     }
 }
