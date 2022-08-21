523cc5ce36b90f352bfd09452089334a760578e6
Anastasiyu <asy.safiulina@mail.ru>
2022-08-21 23:59:38 +0300

    ДЗ Задача 3

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,21 @@ public class Main {
         double x = 5.5;
         char y = 0;
         boolean z = true;
+
+        double oneBoxer = 78.2;
+        double tooBoxer = 82.7;
+        double totalweghf = oneBoxer + tooBoxer;
+        System.out.print("общий вес боксеров" + totalweghf + "кг");
+
+        byte banana = 80;
+        short milk = 105;
+        short iceCream = 100;
+        byte yaytso = 70;
+        var vesZavtraka = banana * 5 + milk * 2 + iceCream * 2 + yaytso * 4;
+        System.out.print("Вес завтрака" + vesZavtraka +"грамм");
+
+        float vesZavtrakaKg = vesZavtraka / 1000;
+        System.out.print("Вес завтрака" + vesZavtrakaKg+ "кг" );
+
     }
 }
